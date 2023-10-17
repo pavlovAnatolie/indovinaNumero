3d22ce45efd68d1d5377fece749dbef6a0bb7d6c
lai2-24 <lai2-24@itismeucci.com>
2023-10-17 13:38:03 +0200

    concluso

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            //inizializzazione di una socket
+            Socket s = new Socket("localhost", 3000);
+
+
+            //apertura del stream input e output
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+             boolean indovinato = false;
+
+             Scanner scrivi = new Scanner(System.in);
+
+            do {
+
+                System.out.println("inserire un numero:");
+                int val = scrivi.nextInt();
+
+                out.writeBytes(val + "\n");
+
+                int temp = Integer.parseInt(in.readLine());
+
+                if(temp == 1){
+                    System.out.println("il valore inserito è troppo piccolo");
+                }else if (temp == 2) {
+                    System.out.println("il valore inserito è troppo grande");
+                }else{
+                    System.out.println("indovinato");
+                    System.out.println("Tentativi: "+Integer.parseInt(in.readLine()));
+                    indovinato = true;
+                }
+                
+                
+            } while (!indovinato);
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,42 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+            try{
+           ServerSocket server = new ServerSocket(3000);
+
+           System.out.println("il server è in ascolto");
+           Socket s = server.accept();
+
+           BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+           DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+           int ranDom = (int)Math.floor(Math.random() *(100- 1 + 1) + 1);
+           System.out.println("il numero generato dal server è: "+ ranDom);
+           boolean mac= false;
+
+                int volte = 0;
+                do {
+                    volte++;
+                    String num = in.readLine();
+                    int numero = Integer.parseInt(num);
+
+
+                    if(numero < ranDom){
+                        out.writeBytes(1+"\n");
+                    }else if(numero > ranDom){
+                        out.writeBytes(2+"\n");
+                    }else{
+                        out.writeBytes(3+"\n");
+                        out.writeBytes(volte+"\n");
+                        mac = true;
+                    }
+                } while (!mac);
+
+                server.close();
+                s.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
